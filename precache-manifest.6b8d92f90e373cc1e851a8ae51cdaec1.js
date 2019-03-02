ee0700902b2c0e4a4c775383fdd80bbb237d1c2d
Mohd <fmohd195@gmail.com>
2019-03-02 19:07:14 +0530

    Updates

---
+++ precache-manifest.6b8d92f90e373cc1e851a8ae51cdaec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d468c204f52b7fe3ca2c",
+    "url": "/PhoneDirectoryApp/static/js/runtime~main.d468c204.js"
+  },
+  {
+    "revision": "b7dda9c088b540e9afa8",
+    "url": "/PhoneDirectoryApp/static/js/main.b7dda9c0.chunk.js"
+  },
+  {
+    "revision": "5ec115eba1d5a3b6af99",
+    "url": "/PhoneDirectoryApp/static/js/2.5ec115eb.chunk.js"
+  },
+  {
+    "revision": "b7dda9c088b540e9afa8",
+    "url": "/PhoneDirectoryApp/static/css/main.a3c4de7d.chunk.css"
+  },
+  {
+    "revision": "38f4f4ba0ffcc9cf9f16586b81412a16",
+    "url": "/PhoneDirectoryApp/index.html"
+  }
+];
